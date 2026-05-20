d001d5ae4c017df9f44f1d95076b9e2d521aa3a0
Dridi14Rahma <dridi.rahma1412@gmail.com>
2026-05-20 15:59:36 +0200

    fix terraform using default vpc and cleanup

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,7 +62,7 @@ data "aws_iam_role" "lab_role" {
 name = "LabRole"
 }
 resource "aws_iam_instance_profile" "lab_profile" {
-name = "lab-instance-profile-v8"
+name = "lab-instance-profile-v6"
 role = data.aws_iam_role.lab_role.name
 }
 # --- EC2 Instances ---
