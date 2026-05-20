390d36c7d5d0f4d4855c9923510cfb738b94a354
Dridi14Rahma <dridi.rahma1412@gmail.com>
2026-05-20 15:34:04 +0200

    fix terraform iam instance profile conflict

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,7 +62,7 @@ data "aws_iam_role" "lab_role" {
 name = "LabRole"
 }
 resource "aws_iam_instance_profile" "lab_profile" {
-name = "lab-instance-profile-v7"
+name = "lab-instance-profile-v8"
 role = data.aws_iam_role.lab_role.name
 }
 # --- EC2 Instances ---
