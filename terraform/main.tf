bfc179c9461aedd2919df4bb0e544dcdeca7a3ca
Dridi14Rahma <dridi.rahma1412@gmail.com>
2026-05-20 13:36:20 +0200

    fix aws restricted lab using default resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,50 +2,37 @@ provider "aws" {
   region = var.aws_region
 }
 
-# ---------------- Security Group ----------------
-resource "aws_security_group" "web_sg" {
-  name = "web-sg"
+data "aws_vpc" "default" {
+  default = true
+}
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "web-sg"
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
 
-# ---------------- EC2 Instances ----------------
+data "aws_security_group" "default" {
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+}
+
 resource "aws_instance" "web" {
   count         = 2
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t3.micro"
 
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  subnet_id              = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [data.aws_security_group.default.id]
 
   tags = {
     Name = "web-${count.index + 1}"
   }
 }
 
-# ---------------- Output ----------------
 output "instance_public_ips" {
   value = aws_instance.web[*].public_ip
 }
