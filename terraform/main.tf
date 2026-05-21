95a1c304f3933d01d05a7ace0dc3c0826fdd9f87
Dridi14Rahma <dridi.rahma1412@gmail.com>
2026-05-21 16:01:31 +0200

    fix: remove yaml callback from ansible.cfg

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,7 +96,7 @@ data "aws_iam_role" "lab_role" {
 }
 
 resource "aws_iam_instance_profile" "lab_profile" {
-  name = "lab-instance-profile-v25"
+  name = "lab-instance-profile-v26"
   role = data.aws_iam_role.lab_role.name
 }
 
